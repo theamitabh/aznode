a41aa2a2809f77b484ecfe121c7b14438eaa793f
theamitabh <amitprem@yahoo.com>
2020-08-03 21:53:20 +0400

    Updated using express & fortune call

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const http = require('http');
+const express = require('express')
+const request = require('request');
+const app = express(); // init express
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("The Third Coming - v3.01!");
-});
+const allEmployees = [
+    { "name": "John", "age": 30, "salary": 2500 },
+    { "name": "Math", "age": 32, "salary": 3500 },
+    { "name": "Smith", "age": 39, "salary": 4500 },
+    { "name": "Derek", "age": 34, "salary": 3000 }
+]
+//route handlers
+app.get('/staff', (req, res) => {
+    res.json({ staff: allEmployees })
+})
 
-const port = process.env.PORT || 8080;
-server.listen(port);
+app.get('/fortune', (req, res) => {
+    //proxy call to yerkee
+    request({
+        uri: 'http://yerkee.com/api/fortune',
+    }).pipe(res)
+})
 
-console.log("Server running at http://localhost:%d", port);
+
+//
+app.listen(process.env.PORT || 80)
+
